f3b006d7d5653ccc05fb7d8c917a61580e0a5c24
Sabyasachi Patra <asabya@gmail.com>
2019-02-18 00:30:25 +0530

    add dedupe

---
+++ 04-dedupe/dedupe.ts
@@ -0,0 +1,17 @@
+import { dedupe } from './mod.ts';
+
+async function main() {
+    var a = [1, 2, 2, 3];
+    var b = dedupe(a);
+    console.log(b);
+
+    var aa = [{a: 2}, {a: 1}, {a: 1}, {a: 1}];
+    var bb = dedupe(aa);
+    console.log(bb);
+
+    var aaa = [{a: 2, b: 1}, {a: 1, b: 2}, {a: 1, b: 3}, {a: 1, b: 4}];
+    var bbb = dedupe(aaa, value => value.a);
+    console.log(bbb);
+}
+
+main();
